ec960689953299380eb4ad1e28b1735bb2fd21f7
Ricardo M <a21310359@ceti.mx>
2021-11-11 20:57:31 -0600

    iniciando prueba3

---
+++ calculadora test.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+<<<<<<< HEAD
+float n, n1, n3, r;
+printf("numero1:");
+=======
+    printf("Escribe el primer numero que quieres sumas:");
+>>>>>>> Prueba1
+}
